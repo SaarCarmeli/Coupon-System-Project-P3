c52f53f10c43896c6f9461b1da48b2ee29ff58a8
Saar <carmeli.saar@gmail.com>
2022-07-25 09:55:23 +0300

    updated singleCompany

--- src/Components/company/singleCompany/singleCompany.tsx
+++ src/Components/company/singleCompany/singleCompany.tsx
@@ -1,5 +1,4 @@
-import { Button, ButtonGroup, Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Typography } from "@mui/material";
 import { Company } from "../../../Models/Company";
 import "./singleCompany.css";
 
@@ -8,19 +7,11 @@ interface SingleCompanyProps {
 }
 
 function SingleCompany(props: SingleCompanyProps): JSX.Element {
-    const navigate = useNavigate();
-    const companyCouponList = () => {
-        
-    }
-
     return (
         <div className="singleCompany Box">
 			<Typography variant="h2">{props.company.id}</Typography><hr/><br/>
             {props.company.name}<br/><br/>
             {props.company.email}<br/><br/>
-            <ButtonGroup variant="contained" fullWidth>
-                <Button color="primary" onClick={companyCouponList}>Coupon List</Button>
-            </ButtonGroup>
         </div>
     );
 }

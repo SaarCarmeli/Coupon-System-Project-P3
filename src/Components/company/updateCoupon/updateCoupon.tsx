99a61b3cc20b04b6cc251363d066ffebfada03f1
Saar <carmeli.saar@gmail.com>
2022-07-25 09:02:32 +0300

    finished updateCoupon

--- src/Components/company/updateCoupon/updateCoupon.tsx
+++ src/Components/company/updateCoupon/updateCoupon.tsx
@@ -16,7 +16,7 @@ import store from "../../../redux/store";
 function UpdateCoupon(): JSX.Element {
   const navigate = useNavigate();
   let [coupons, setCoupons] = useState<Coupon[]>([]);
-  let [updateCoupon, setUpdateCoupon] = useState<Coupon>(new Coupon());
+  let [updCoupon, setUpdateCoupon] = useState<Coupon>(new Coupon());
   let [couponId, setId] = useState(0);
   const {
     register,
@@ -31,24 +31,45 @@ function UpdateCoupon(): JSX.Element {
   });
 
   const getCoupon = () => {
-    setUpdateCoupon(coupons.filter((item) => {
-      return (item.id === couponId);
-    }).shift());
+    setUpdateCoupon(
+      coupons
+        .filter((item) => {
+          return item.id === couponId;
+        })
+        .shift()
+    );
   };
 
   const sendUpdate = (msg: Coupon) => {
-    // msg.id = updateCompany.id;
-    // msg.password = updateCompany.password;
-    // msg.coupons = updateCompany.coupons;
-    // if (msg.name == "") {
-    //   msg.name = updateCompany.name;
-    // }
-    // if (msg.email == "") {
-    //   msg.name = updateCompany.email;
-    // }
+    msg.id = updCoupon.id;
+    if (msg.amount === 0) {
+      msg.amount = updCoupon.amount;
+    }
+    if (msg.price === 0) {
+      msg.price = updCoupon.price;
+    }
+    if (msg.category === "") {
+      msg.category = updCoupon.category;
+    }
+    if (msg.title === "") {
+      msg.title = updCoupon.title;
+    }
+    if (msg.description === "") {
+      msg.description = updCoupon.description;
+    }
+    if (msg.image === "") {
+      msg.image = updCoupon.image;
+    }
+    if (msg.startDate === "") {
+      msg.startDate = updCoupon.startDate;
+    }
+    if (msg.endDate === "") {
+      msg.endDate = updCoupon.endDate;
+    }
     jwtAxios
       .put<Coupon>(globals.urls.companyUpdateCoupon, msg)
       .then((response) => {
+        store.dispatch(updateCoupon(msg));
         notify.success(SccMsg.COUPON_UPDATED);
       })
       .catch((err) => {
@@ -65,7 +86,7 @@ function UpdateCoupon(): JSX.Element {
     <div className="updateCoupon" dir="ltr">
       <>
         <Typography variant="h4" align="center">
-          Update Coupon #{updateCoupon.id}
+          Update Coupon #{updCoupon.id}
         </Typography>
         <hr />
         <br />
@@ -91,27 +112,87 @@ function UpdateCoupon(): JSX.Element {
         </ButtonGroup>
         <br />
         <br />
-        {<SingleCoupon coupon={updateCoupon} />}
+        {<SingleCoupon coupon={updCoupon} />}
         <br />
         <br />
         <form onSubmit={handleSubmit(sendUpdate)}>
           <TextField
-            name="name"
-            label="Name"
+            name="amount"
+            label="Amount"
             variant="outlined"
             className="TextBox"
             fullWidth
-            {...register("name")}
+            {...register("amount")}
           />
           <br />
           <br />
           <TextField
-            name="email"
-            label="Email"
+            name="price"
+            label="Price"
             variant="outlined"
             className="TextBox"
             fullWidth
-            {...register("email")}
+            {...register("price")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="category"
+            label="Category"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("category")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="title"
+            label="Title"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("title")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="description"
+            label="Description"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("description")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="image"
+            label="Image"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("image")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="startDate"
+            label="Start date"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("startDate")}
+          />
+          <br />
+          <br />
+          <TextField
+            name="endDate"
+            label="End date"
+            variant="outlined"
+            className="TextBox"
+            fullWidth
+            {...register("endDate")}
           />
           <br />
           <br />
